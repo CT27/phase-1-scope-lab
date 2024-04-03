c5c1d9e64e1b6920fd705a5e1a63fb9a4dc1fa04
Candice Thomason <candice.thomason@gmail.com>
2024-04-03 15:03:14 +1100

    send all to git

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+//1.
+var customerName = "bob";
+
+//2.
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+//3.
+function setBestCustomer() {
+  return (bestCustomer = "not bob");
+}
+
+setBestCustomer();
+
+//4.
+function overwriteBestCustomer() {
+  return (bestCustomer = "maybe bob");
+}
+
+overwriteBestCustomer();
+
+//5.
+const leastFavouriteCustomer = "Jane";
+
+function changeLeastFavouriteCustomer() {
+  return (leastFavouriteCustomer = "Rodney");
+}
+
+changeLeastFavouriteCustomer();
